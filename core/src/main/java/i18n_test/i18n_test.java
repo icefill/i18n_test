b1d6822e18757d1b454d05fa195343da7510c845
icefill-PC <icefill@bitbucket.org>
2017-11-13 23:15:57 +0900

    Fisrt commit

---
+++ core/src/main/java/i18n_test/i18n_test.java
@@ -0,0 +1,7 @@
+package i18n_test;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class i18n_test extends ApplicationAdapter {
+}

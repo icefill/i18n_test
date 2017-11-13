c8f87e41902fbc738462f0e527ec0f0adc159bd8
icefill-PC <icefill@bitbucket.org>
2017-11-14 02:22:49 +0900

    deleted wrong project

--- core/src/main/java/i18n_test/i18n_test.java
+++
@@ -1,7 +0,0 @@
-package i18n_test;
-
-import com.badlogic.gdx.ApplicationAdapter;
-
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class i18n_test extends ApplicationAdapter {
-}
